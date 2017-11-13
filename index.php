6c124cbf5ca08e6e3582ce3c0c9011e4cbb177e4
Unknown <nsehall.coding@gmail.com>
2017-11-14 06:02:36 +1000

    Early Design Complete
    Early Design stage completed. About to start inplementing JS.

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
  
 <head>
  
+ <meta charset="UTF-8"/>
   <title>Ping Pong Scoreboard</title>
   <link rel="stylesheet" type="text/css" href="style.css" />
+  <?php
+  	$p1Score = 0;
+  	$p2Score = 0;
+  ?>
  
 </head>
  
@@ -15,13 +20,13 @@
  
     <div id="PlayerNames" class="Wrappers">
  
-      <div id="Player1Name" class="LeftSections">
+      <div id="Player1Name">
  
         <h1>Player 1</h1>
  
       </div>
  
-      <div id="Player2Name" class="RightSections">
+      <div id="Player2Name">
  
         <h1>Player 2</h1>
  
@@ -32,33 +37,33 @@
     <div id="LowerContent">
 	    <div id="LeftPlayerContent" class="Wrappers">
 	 
-	      <div id="Player1Score" class="LeftSections">
+	      <div id="Player1Score">
 	 
 	        <h1>0</h1>
 	 
 	      </div>
 	 
 
-	 	  <div id="Player1Buttons" class="LeftSections">
+	 	  <div id="Player1Buttons">
 	 
-	        <button type="button" class="Buttons PlusButton">+</button>
-	        <button type="button" class="Buttons">-</button>
+	        <button type="button" class="Buttons PlusButton" name="P1PlusButton">+</button>
+	        <button type="button" class="Buttons" name="P1MinusButton">-</button>
 
 	      </div>
 	      
 	 	</div>
 	    <div id="RightPlayerContent" class="Wrappers">
 	 	  
-	 	  <div id="Player2Score" class="RightSections">
+	 	  <div id="Player2Score">
 	 
 	        <h1>0</h1>
 	 
 	      </div>
 
-	      <div id="Player2Buttons" class="RightSections">
+	      <div id="Player2Buttons">
 	 
-	        <button type="button" class="Buttons PlusButton">+</button>
-	        <button type="button" class="Buttons">-</button>
+	        <button type="button" class="Buttons PlusButton" name="P2PlusButton">+</button>
+	        <button type="button" class="Buttons" name="P2MinusButton">-</button>
 	 
 	      </div>
 		 
